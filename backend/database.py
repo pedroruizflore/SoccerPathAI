ae0d455cbad1e19946b1c5741446feeba4718d6f
Pedro Henrique <pedrohenrique@MacBook-Air-de-Pedro.local>
2026-04-26 19:03:23 -0500

    Add backend to sys.path

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,10 @@
 import psycopg2
+import os
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="soccerpath_db",
-        user="pedrohenrique",
-        password="" 
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST", "localhost"),
+        database=os.getenv("DB_NAME", "soccer_db"),
+        user=os.getenv("DB_USER", "postgres"),
+        password=os.getenv("DB_PASS", "password") 
     )
-    return conn
